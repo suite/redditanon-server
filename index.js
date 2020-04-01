13de955f22a436c84f37d2759366995c86e3d75f
Matt <gr8bitmail@gmail.com>
2020-04-01 17:11:00 -0500

    final changes, working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const RedditClient = require("./redditapi/reddit");
+const { body, validationResult } = require("express-validator");
 
 const express = require("express");
 const app = express();
@@ -6,40 +7,41 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.get("/", (req, res) => res.status(200).send("Hello World!"));
 
-app.get("/", (req, res) => res.status(204).send("Hello World!"));
+app.post(
+  "/",
+  [
+    body("contentText")
+      .not()
+      .isEmpty(),
+    body("contentId")
+      .not()
+      .isEmpty(),
+    body("selectValue")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
 
-app.post("/", async (req, res) => {
-  /*
-    {
-    contentObj: {
-      contentText: 'dsfdsf',
-      contentId: 't3_frtzfr'
-    },
-    selectValue: 'random'
+    //Must be random... In future add other options?
+    if (req.body.selectValue != "random") {
+      return res.status(422).json({ errors: ["Unknown select value"] });
+    }
+
+    try {
+      let url = await new RedditClient().commentRandom(req.body);
+      console.log("server got url", url);
+      res.status(200).json({ url });
+    } catch (err) {
+      console.log("Error getting url", err.message);
+      return res.status(422).json({ errors: [err.message] });
+    }
   }
-  */
-
-  if (!req.body.contentObj || !req.body.selectValue) {
-    return res.status("404").send({ status: "errror" });
-  }
-
-  try {
-    await new RedditClient().comment(req.body);
-    console.log(req.body); // your JSON
-    return res.status(200).send(req.body);
-  } catch (err) {
-    return res.status("404").send({ status: "errror" });
-  }
-  // echo the result back
-});
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- redditapi/reddit.js
+++ redditapi/reddit.js
@@ -1,9 +1,88 @@
 const rp = require("request-promise");
 
 module.exports = class RedditClient {
-  constructor() {}
+  constructor() {
+    this.accounts = [
+      { username: "altinator5000", password: "SDFkl324" },
+      { username: "notanalt500", password: "FDdf77333" }
+    ];
+  }
 
-  async comment(data) {
-    console.log("commenting");
+  async login(account) {
+    const opts = {
+      url: "https://www.reddit.com/loginproxy",
+      method: "POST",
+      headers: {
+        origin: "https://www.reddit.com",
+        referer: "https://www.reddit.com/login",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Mobile Safari/537.36"
+      },
+      json: {
+        username: account["username"],
+        password: account["password"],
+        otp: ""
+      }
+    };
+
+    try {
+      const body = await rp(opts);
+      return body["session"]["accessToken"];
+    } catch (err) {
+      throw Error(`Error logging into ${account["username"]} ${err.message}`);
+    }
+  }
+
+  async commentRandom(data) {
+    let { contentText, contentId, selectValue } = data;
+
+    const randAccount = this.accounts[
+      Math.floor(Math.random() * this.accounts.length)
+    ];
+
+    console.log(`Using ${randAccount["username"]}...`);
+
+    let accessToken = await this.login(randAccount);
+
+    console.log("Collected accessToken", accessToken);
+
+    const opts = {
+      url:
+        "https://oauth.reddit.com/api/comment?redditWebClient=mweb2x&layout=classic&allow_over18=&app=2x-client-production&obey_over18=true&raw_json=1",
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Mobile Safari/537.36"
+      },
+      formData: {
+        api_type: "json",
+        thing_id: contentId,
+        text: contentText,
+        raw_json: "1"
+      },
+      resolveWithFullResponse: true
+    };
+
+    try {
+      let resp = await rp(opts);
+      resp = JSON.parse(resp.body);
+
+      console.log(JSON.stringify(resp["json"]["errors"]));
+      if (resp["json"]["errors"].length > 0) {
+        throw Error("Rate limited");
+      }
+
+      console.log(
+        "reply resp",
+        resp["json"]["data"]["things"][0]["data"]["permalink"]
+      );
+
+      return resp["json"]["data"]["things"][0]["data"]["permalink"];
+    } catch (err) {
+      throw Error(err);
+    }
   }
 };
